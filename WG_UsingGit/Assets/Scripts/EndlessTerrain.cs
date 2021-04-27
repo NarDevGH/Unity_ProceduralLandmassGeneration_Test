558ff6869639e0ebf4c94fe886e64c8533749c00
NarMaroth <bautista.mateo@outlook.com>
2021-04-27 12:25:18 -0300

    Fixed Chunks staying visible

--- WG_UsingGit/Assets/Scripts/EndlessTerrain.cs
+++ WG_UsingGit/Assets/Scripts/EndlessTerrain.cs
@@ -20,7 +20,7 @@ public class EndlessTerrain : MonoBehaviour {
 	int chunksVisibleInViewDst;
 
 	Dictionary<Vector2, TerrainChunk> terrainChunkDictionary = new Dictionary<Vector2, TerrainChunk>();
-	List<TerrainChunk> terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
+	static List<TerrainChunk> terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
 
 	void Start() {
 		mapGenerator = FindObjectOfType<MapGenerator> ();
@@ -57,9 +57,6 @@ public class EndlessTerrain : MonoBehaviour {
 
 				if (terrainChunkDictionary.ContainsKey (viewedChunkCoord)) {
 					terrainChunkDictionary [viewedChunkCoord].UpdateTerrainChunk ();
-					if (terrainChunkDictionary [viewedChunkCoord].IsVisible ()) {
-						terrainChunksVisibleLastUpdate.Add (terrainChunkDictionary [viewedChunkCoord]);
-					}
 				} else {
 					terrainChunkDictionary.Add (viewedChunkCoord, new TerrainChunk (viewedChunkCoord, chunkSize, detailLevels, transform, mapMaterial));
 				}
@@ -147,6 +144,8 @@ public class EndlessTerrain : MonoBehaviour {
 							lodMesh.RequestMesh(mapData);
 						}
 					}
+
+					terrainChunksVisibleLastUpdate.Add(this);
 
 				}
 
